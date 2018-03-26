01fda1e21cb25dcb4c16b3582d5918031af0d15c
semernitskaya <semernitskaya@gmail.com>
2018-03-26 19:17:06 +0300

    basic types

---
+++ src/com/ol/basicTypes.kt
@@ -0,0 +1,23 @@
+package com.ol
+
+/**
+ * Created by Semernitskaya on 26.03.2018.
+ */
+
+fun main(vararg args: String) {
+    val i = 6
+    println(i.javaClass)
+
+    val k: Long = 7
+    println(k.javaClass)
+
+    val m = 9199999999999999999
+    println(m.javaClass)
+
+    val b1 = 0b0101
+    val b2 = 0b1001
+
+    println(b1 xor b2)
+
+}
+
